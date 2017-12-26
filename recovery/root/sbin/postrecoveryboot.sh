2d8ab1957c03ee6baa601e866ca2b7af7facee78
timesleader <1449626263@qq.com>
2017-12-26 13:38:32 +0800

    remove twrp ota app

--- recovery/root/sbin/postrecoveryboot.sh
+++ recovery/root/sbin/postrecoveryboot.sh
@@ -5,6 +5,4 @@ setprop ro.build.fingerprint $finger
 finger=$(cat /system/build.prop | grep "ro.build.date.utc=" | dd bs=1 skip=18)
 setprop ro.build.date.utc $finger
 mv /system/recovery-from-boot.bak /system/recovery-from-boot.p
-mv /shuajizu/ota.apk /data/app/ota.apk
-chmod 0644 /data/app/ota.apk
 umount /system
