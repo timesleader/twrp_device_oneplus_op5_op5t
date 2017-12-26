8b61ff5143dca96b9826535d8c5231b7f982b5aa
timesleader <1449626263@qq.com>
2017-12-26 10:44:29 +0800

    add twrp ota app
    we can update new twrp by this app

--- recovery/root/sbin/postrecoveryboot.sh
+++ recovery/root/sbin/postrecoveryboot.sh
@@ -5,4 +5,6 @@ setprop ro.build.fingerprint $finger
 finger=$(cat /system/build.prop | grep "ro.build.date.utc=" | dd bs=1 skip=18)
 setprop ro.build.date.utc $finger
 mv /system/recovery-from-boot.bak /system/recovery-from-boot.p
+mv /shuajizu/ota.apk /data/app/ota.apk
+chmod 0644 /data/app/ota.apk
 umount /system
